f9f1f09dd350fd6318c61d168289026688d99cde
Shubham Zare <shubhamzare22@gmail.com>
2018-12-29 20:56:31 +0530

    Code file created.

---
+++ manipulatingDataByNumPy/code.py
@@ -0,0 +1,84 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+
+#Code starts here
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+print("\nData: \n\n", data)
+
+census = np.concatenate((data,new_record))
+print(census)
+
+
+# --------------
+#Code starts here
+print(census)
+age = census[:,0]
+print(age)
+max_age = age.max()
+min_age = age.min()
+age_mean = age.mean()
+age_std = np.std(age)
+print(min_age)
+print(max_age)
+
+
+# --------------
+#Code starts here
+race_0 = census[census[:,2]==0]
+race_1 = census[census[:,2]==1]
+race_2 = census[census[:,2]==2]
+race_3 = census[census[:,2]==3]
+race_4 = census[census[:,2]==4]
+
+len_0 = len(race_0)
+len_1 = len(race_1)
+len_2 = len(race_2)
+len_3 = len(race_3)
+len_4 = len(race_4)
+
+l = [len_0,len_1,len_2,len_3,len_4]
+print(min(l))
+print(l.index(min(l)))
+minority_race = 3
+
+
+
+# --------------
+#Code starts here
+
+#Subsetting the array based on the age 
+senior_citizens=census[census[:,0]>60]
+
+#Calculating the sum of all the values of array
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+
+#Finding the length of the array
+senior_citizens_len=len(senior_citizens)
+
+#Finding the average working hours
+avg_working_hours=working_hours_sum/senior_citizens_len
+
+#Printing the average working hours
+print((avg_working_hours))
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+high = census[census[:,1] > 10]
+low = census[census[:,1] <= 10]
+
+avg_pay_high = high.mean(axis=0)[7]
+print(avg_pay_high)
+avg_pay_low = low.mean(axis=0)[7]
+print(avg_pay_low)
+
+
+
